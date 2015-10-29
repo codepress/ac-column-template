031a5a980fef949565e59cbe1804056d8019d8c6
Tobias Schutter <tschutter@gmail.com>
2015-10-29 11:57:45 +0100

    Improved editable and sorting support

--- ac-column-COLUMN_NAME.php
+++ ac-column-COLUMN_NAME.php
@@ -2,161 +2,206 @@
 
 class CPAC_Column_COLUMN_NAME extends CPAC_Column {
 
-    /**
-     * This function will setup the column data
-     *
-     */
-    public function init() {
+	/**
+	 * This function will setup the column data
+	 *
+	 */
+	public function init() {
 
-        // do not delete
-    	parent::init();
+		// do not delete
+		parent::init();
 
-        // Identifier, pick an unique name. Single word, no spaces. Underscores allowed.
-        $this->properties['type'] = 'column-COLUMN_NAME';
+		// Identifier, pick an unique name. Single word, no spaces. Underscores allowed.
+		$this->properties['type'] = 'column-COLUMN_NAME';
 
-        // Default column label.
-        $this->properties['label'] = __( 'COLUMN_LABEL', 'ac-COLUMN_NAME' );
+		// Default column label.
+		$this->properties['label'] = __( 'COLUMN_LABEL', 'ac-COLUMN_NAME' );
 
-        // (optional) You can make it support sorting with the pro add-on enabled. Sorting will be done by it's raw value.
-        $this->properties['is_sortable'] = true;
 
-        // (optional) Enable/Disable inline editing support for this column.
-        $this->properties['is_editable'] = false;
-    }
+		// Optional settings
 
-    /**
-     * Returns the display value for the column.
-     *
-     * @param int $id ID
-     * @return string Value
-     */
-    public function get_value( $post_id ) {
+		// (optional) You can make it support sorting with the pro add-on enabled. Sorting will be done by 'get_sorting_value()'.
+		$this->properties['is_sortable'] = true;
 
-        // get raw value
-        $value = $this->get_raw_value( $post_id );
+		// (optional) Set how the column should be sorted, this can either be 'numeric' or 'regular'. Default is 'regular'. Only applies if 'is_sortable' is 'true'.
+		$this->properties['sort_type'] = 'regular';
 
-        // optionally you can change the display of the value. In this example we added a post link.
-        $value = '<a href="' . get_permalink( $post_id ) . '">' . $value . '</a>';
+		// (optional) Enable/Disable inline editing support for this column. For it's editable settings use 'get_editable_settings()'.
+		$this->properties['is_editable'] = false;
+	}
 
-        return $value;
-    }
+	/**
+	 * Returns the display value for the column.
+	 *
+	 * @param int $id ID
+	 * @return string Value
+	 */
+	public function get_value( $post_id ) {
 
-    /**
-     * Get the raw, underlying value for the column
-     * Not suitable for direct display, use get_value() for that
-     * This value will be used by 'sorting', 'inline-edit' and get_value().
-     *
-     * @param int $id ID
-     * @return mixed Value
-     */
-    public function get_raw_value( $post_id ) {
+		// get raw value
+		$value = $this->get_raw_value( $post_id );
 
-        // put all the column logic here to retrieve the value you need
-        // For example: $value = get_post_meta( $post_id, '_my_custom_field_example', true );
+		// optionally you can change the display of the value. In this example we added a post link.
+		$value = '<a href="' . get_permalink( $post_id ) . '">' . $value . '</a>';
 
-        $value = 'something';
+		return $value;
+	}
 
-        return $value;
-    }
+	/**
+	 * Get the raw, underlying value for the column
+	 * Not suitable for direct display, use get_value() for that
+	 * This value will be used by 'inline-edit' and get_value().
+	 *
+	 * @param int $id ID
+	 * @return mixed Value
+	 */
+	public function get_raw_value( $post_id ) {
 
-    /**
-     * (Optional) Apply conditionals. You can remove this function is you do not use it!
-     *
-     * This determines whether the column should be available. If you want to disable this column
-     * for a particular posttype you can set this to false.
-     *
-     * @return bool True/False Default should be 'true'.
-     */
-    public function apply_conditional() {
+		// put all the column logic here to retrieve the value you need
+		// For example: $value = get_post_meta( $post_id, '_my_custom_field_example', true );
 
-        // Example: if the posttype does not support thumbnails then return false
-        // if ( ! post_type_supports( $this->get_post_type(), 'thumbnail' ) ) {
-        //    return false;
-        // }
+		$value = 'something';
 
-        return true;
-    }
+		return $value;
+	}
 
-    /**
-     * (Optional) Inline editing settings. You can remove this function is you do not use it!
-     *
-     * @return array Editable settings
-     */
-    public function get_editable_settings() {
+	/**
+	 * (Optional) This value will be used for 'sorting' only. You can remove this function is you do not use it!
+	 *
+	 * The value should either be a string or integer. No arrays or objects.
+	 *
+	 * @param int $id ID
+	 * @return string|int Value
+	 */
+	public function get_sorting_value( $post_id ) {
 
-        // available types: text, textarea, media, float, togglable, select, select2_dropdown and select2_tags
-        $settings = array(
-            'type' => 'text'
-        );
+		// put all the column logic here to retrieve the value you need
+		// For example: $value = get_post_meta( $post_id, '_my_custom_field_example', true );
 
-        // (Optional) Only applies to type: togglable, select, select2_dropdown and select2_tags
-        // $settings['options'] = array( 'value_1', 'value_2', 'etc.' );
+		$value = $this->get_raw_value();
 
-        // (Optional) If a selector is provided, editable will be delegated to the specified targets
-        // $settings['js']['selector'] = 'a.my-class';
+		return $value;
+	}
 
-        return $settings;
-    }
+	/**
+	 * (Optional) Apply conditionals. You can remove this function is you do not use it!
+	 *
+	 * This determines whether the column should be available. If you want to disable this column
+	 * for a particular posttype you can set this to false.
+	 *
+	 * @return bool True/False Default should be 'true'.
+	 */
+	public function apply_conditional() {
 
-    /**
-     * (Optional) Saves the value after using inline-edit. You can remove this function is you do not use it!
-     *
-     * @param int $id Object ID
-     * @param mixed $value Value to be saved
-     */
-    public function save( $id, $value ) {
+		// Example: if the posttype does not support thumbnails then return false
+		// if ( ! post_type_supports( $this->get_post_type(), 'thumbnail' ) ) {
+		//    return false;
+		// }
 
-        // Store the value that has been entered with inline-edit
-        // For example: update_post_meta( $id, '_my_custom_field_example', $value );
-    }
+		return true;
+	}
 
-    /**
-     * (Optional) Create extra settings for you column. These are visible when editing a column. You can remove this function is you do not use it!
-     *
-     * Write your own settings or use any of the standard avaiable settings.
-     */
-    public function display_settings() {
+	/**
+	 * (Optional) Inline editing settings. You can remove this function is you do not use it!
+	 *
+	 * @return array Editable settings
+	 */
+	public function get_editable_settings() {
 
-        // You can write your own input fields here, or use the examples below...
+		// available types: text, textarea, media, float, togglable, select, select2_dropdown and select2_tags
+		$settings = array(
+			'type' => 'text'
+		);
 
-        // The following settings are available out-of-the-box:
+		// (Optional) Only applies to type: togglable, select, select2_dropdown and select2_tags
+		// $settings['options'] = array( 'value_1', 'value_2', 'etc.' );
 
-        // Display an image preview size settings screen
-        // $this->display_field_preview_size();
+		// (Optional) If a selector is provided, editable will be delegated to the specified targets
+		// $settings['js']['selector'] = 'a.my-class';
 
-        // Display an excerpt length input field
-        // $this->display_field_excerpt_length();
+		// (Optional) Only applies to the type 'select2_dropdown'. Populates the available select2 dropdown values through ajax.
+		// Ajax callback used is 'get_editable_ajax_options()'.
+		// $settings['ajax_populate'] = true;
 
-        // Display a date format settings input field
-        // $this->display_field_date_format();
+		// (Optional) Only applies to the type 'select2_dropdown'. Determines how the options should be displayed. Default is empty. Options are 'post', 'user' or 'wc_product'.
+		// $settings['formatted_value'] = 'post';
 
-        // Display before and after input fields
-        // $this->display_field_before_after();
+		return $settings;
+	}
 
-        // Displays a dropdown menu with user display formats
-        // $this->display_field_user_format();
+	/**
+	 * (Optional) Saves the value after using inline-edit. You can remove this function is you do not use it!
+	 *
+	 * @param int $id Object ID
+	 * @param mixed $value Value to be saved
+	 */
+	public function save( $id, $value ) {
 
-        // Displays a text field
-        // $this->display_field_text( $option_name, $label, $description );
+		// Store the value that has been entered with inline-edit
+		// For example: update_post_meta( $id, '_my_custom_field_example', $value );
+	}
 
-        // Displays a select field
-        // $this->display_field_select( $option_name, $label, $options, $description );
-    }
+	/**
+	 * (Optional) Get the filtering options for when ajax_populate is 'true'. You can remove this function is you do not use it!
+	 *
+	 * @param string $searchterm Search term provided by the user
+	 */
+	public function get_editable_ajax_options( $searchterm ) {
 
-    /*
-     * (Optional) Enqueue CSS + JavaScript on the admin listings screen. You can remove this function is you do not use it!
-     *
-     * This action is called in the admin_head action on the listings screen where your column values are displayed.
-     * Use this action to add CSS + JavaScript
-     *
-     * @since 3.3.4
-     */
-    public function scripts() {
+		$options = array();
 
-        // wp_register_script( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "js/column.js" );
-        // wp_enqueue_script( 'ac-COLUMN_NAME' );
+		// $options = $this->get_editable()->get_posts_options( array( 's' => $searchterm, 'post_type' => 'post' ) );
 
-        // wp_register_style( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "css/column.css" );
-        // wp_enqueue_style( 'ac-COLUMN_NAME' );
-    }
+		return $options;
+	}
+
+	/**
+	 * (Optional) Create extra settings for you column. These are visible when editing a column. You can remove this function is you do not use it!
+	 *
+	 * Write your own settings or use any of the standard avaiable settings.
+	 */
+	public function display_settings() {
+
+		// You can write your own input fields here, or use the examples below...
+
+		// The following settings are available out-of-the-box:
+
+		// Display an image preview size settings screen
+		// $this->display_field_preview_size();
+
+		// Display an excerpt length input field
+		// $this->display_field_excerpt_length();
+
+		// Display a date format settings input field
+		// $this->display_field_date_format();
+
+		// Display before and after input fields
+		// $this->display_field_before_after();
+
+		// Displays a dropdown menu with user display formats
+		// $this->display_field_user_format();
+
+		// Displays a text field
+		// $this->display_field_text( $option_name, $label, $description );
+
+		// Displays a select field
+		// $this->display_field_select( $option_name, $label, $options, $description );
+	}
+
+	/*
+	 * (Optional) Enqueue CSS + JavaScript on the admin listings screen. You can remove this function is you do not use it!
+	 *
+	 * This action is called in the admin_head action on the listings screen where your column values are displayed.
+	 * Use this action to add CSS + JavaScript
+	 *
+	 * @since 3.3.4
+	 */
+	public function scripts() {
+
+		// wp_register_script( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "js/column.js" );
+		// wp_enqueue_script( 'ac-COLUMN_NAME' );
+
+		// wp_register_style( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "css/column.css" );
+		// wp_enqueue_style( 'ac-COLUMN_NAME' );
+	}
 }
