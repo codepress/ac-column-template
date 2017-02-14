4bbadef4284e1416029f23ddebf1bc3a1a8f8e38
Tobias Schutter <tschutter@gmail.com>
2017-02-14 16:12:20 +0100

    Pro column support

--- ac-COLUMN_NAME.php
+++ ac-COLUMN_NAME.php
@@ -12,18 +12,34 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 // 1. Set text domain
-// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'ac-COLUMN_NAME', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+/* @link https://codex.wordpress.org/Function_Reference/load_plugin_textdomain */
+load_plugin_textdomain( 'ac-COLUMN_NAME', false, plugin_dir_path( __FILE__ ) . '/languages/' );
 
 // 2. Set the type of column.
 // Use the type: 'post', 'user', 'comment', 'media' or 'taxonomy'.
-add_filter( 'cac/columns/custom/type=post', 'cac_register_column_COLUMN_NAME' );
+add_action( 'ac/column_types/post', 'ac_register_column_COLUMN_NAME' );
 
 // 3. Register the column.
-function cac_register_column_COLUMN_NAME( $columns ) {
+function ac_register_column_COLUMN_NAME( AC_ListScreen $list_screen ) {
 
-    // Class name and absolute filepath of the new column
-    $columns['CPAC_Column_COLUMN_NAME'] = plugin_dir_path( __FILE__ ) . 'ac-column-COLUMN_NAME.php';
+	require_once plugin_dir_path( __FILE__ ) . 'ac-column-COLUMN_NAME.php';
 
-    return $columns;
+	$list_screen->register_column_type( new AC_Column_COLUMN_NAME );
+}
+
+// -------------------------------------- //
+// This part is for the PRO version only. //
+// -------------------------------------- //
+
+// 4. (Optional) Set the type of PRO column.
+// Use the type: 'post', 'user', 'comment', 'media' or 'taxonomy'.
+add_action( 'acp/column_types/post', 'ac_register_pro_column_COLUMN_NAME' );
+
+// 5. (Optional) Register the PRO column.
+function ac_register_pro_column_COLUMN_NAME( AC_ListScreen $list_screen ) {
+
+	require_once plugin_dir_path( __FILE__ ) . 'ac-column-COLUMN_NAME.php';
+	require_once plugin_dir_path( __FILE__ ) . 'acp-column-COLUMN_NAME.php';
+
+	$list_screen->register_column_type( new ACP_Column_COLUMN_NAME );
 }
--- ac-column-COLUMN_NAME.php
+++ ac-column-COLUMN_NAME.php
@@ -1,5 +1,122 @@
 <?php
 
+class AC_Column_COLUMN_NAME extends AC_Column {
+
+	public function __construct() {
+
+		// Identifier, pick an unique name. Single word, no spaces. Underscores allowed.
+		$this->set_type( 'column-COLUMN_NAME' );
+
+		// Default column label.
+		$this->set_label( __( 'COLUMN_LABEL', 'ac-COLUMN_NAME' ) );
+	}
+
+	/**
+	 * Returns the display value for the column.
+	 *
+	 * @param int $id ID
+	 * @return string Value
+	 */
+	public function get_value( $post_id ) {
+
+		// get raw value
+		$value = $this->get_raw_value( $post_id );
+
+		// optionally you can change the display of the value. In this example we added a post link.
+		$value = '<a href="' . esc_url( get_permalink( $post_id ) ) . '">' . $value . '</a>';
+
+		return $value;
+	}
+
+	/**
+	 * Get the raw, underlying value for the column
+	 * Not suitable for direct display, use get_value() for that
+	 * This value will be used by 'inline-edit' and get_value().
+	 *
+	 * @param int $id ID
+	 * @return mixed Value
+	 */
+	public function get_raw_value( $post_id ) {
+
+		// put all the column logic here to retrieve the value you need
+		// For example: $value = get_post_meta( $post_id, '_my_custom_field_example', true );
+
+		$value = 'something';
+
+		return $value;
+	}
+
+	/**
+	 * (Optional) Create extra settings for you column. These are visible when editing a column. You can remove this function is you do not use it!
+	 *
+	 * Write your own settings or use any of the standard available settings.
+	 */
+	protected function register_settings() {
+
+		// Display an image preview size settings screen
+		$this->add_setting( new AC_Settings_Setting_Image( $this ) );
+
+		// Display an excerpt length input field
+		$this->add_setting( new AC_Settings_Setting_WordLimit( $this ) );
+		$this->add_setting( new AC_Settings_Setting_CharacterLimit( $this ) );
+
+		// Display a date format settings input field
+		$this->add_setting( new AC_Settings_Setting_Date( $this ) );
+
+		// Display before and after input fields
+		$this->add_setting( new AC_Settings_Setting_BeforeAfter( $this ) );
+
+		// Displays a dropdown menu with user display formats
+		$this->add_setting( new AC_Settings_Setting_User( $this ) );
+
+		// Displays a dropdown menu with post display formats
+		$this->add_setting( new AC_Settings_Setting_Post( $this ) );
+
+		// Displays a text field
+		// TODO
+		// $this->display_field_text( $option_name, $label, $description );
+
+		// Displays a select field
+		// TODO
+		// $this->display_field_select( $option_name, $label, $options, $description );
+	}
+
+	/**
+	 * (Optional) Is valid. You can remove this function is you do not use it!
+	 *
+	 * This determines whether the column should be available. If you want to disable this column
+	 * for a particular post type you can set this to false.
+	 *
+	 * @return bool True/False Default should be 'true'.
+	 */
+	public function is_valid() {
+
+		// Example: if the post type does not support thumbnails then return false
+		// if ( ! post_type_supports( $this->get_post_type(), 'thumbnail' ) ) {
+		//    return false;
+		// }
+
+		return true;
+	}
+
+	/*
+	 * (Optional) Enqueue CSS + JavaScript on the admin listings screen. You can remove this function is you do not use it!
+	 *
+	 * This action is called in the admin_head action on the listings screen where your column values are displayed.
+	 * Use this action to add CSS + JavaScript
+	 */
+	public function scripts() {
+
+		// wp_enqueue_script( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "js/column.js" );
+		// wp_enqueue_style( 'ac-COLUMN_NAME', plugin_dir_url( __FILE__ ) . "css/column.css" );
+	}
+
+}
+
+return;
+
+// TODO: remove
+
 class CPAC_Column_COLUMN_NAME extends CPAC_Column {
 
 	/**
