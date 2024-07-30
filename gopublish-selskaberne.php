d8bfad4e32b534424ff573d5615c2e1e565830cb
Retrofitterdk <sbn@retrofitter.dk>
2024-07-30 17:25:35 +0200

    Update specialty bearing status for persons
    whenever either a person or an organization are updated.

--- gopublish-selskaberne.php
+++ gopublish-selskaberne.php
@@ -71,39 +71,78 @@ add_action('acp/column_types', static function (AC\ListScreen $list_screen): voi
 // 2. Optionally: load a text domain
 load_plugin_textdomain('gopublish-selskaberne', false, __DIR__ . '/languages/');
 
-
+// Get IDs of associated organizations
 function gp_get_associated_organization_ids($postID) {
 
     $associatedPostIds = array();
-
-	$terms = get_the_terms(get_the_ID(),'organization_connect');
+    // Get organization terms of post
+	$terms = get_the_terms($postID,'organization_connect');
+    // Get IDs of posts associated with organization terms
 		if ($terms) {
     		foreach($terms as $term){
 	    		$termID = ($term->term_id);
 		    	$associatedPostIds[] = ShadowTerms\API\get_post_id($termID);
-            // $postID = ShadowTerms\API\get_post_id($termID);
-            // $specialty_bearing[] = (bool) get_post_meta( $postID, 'organization_specialty_bearing', true );
 		    }
-        // if (in_array(true, $specialty_bearing)) {
-		// 	return true;
-		// } else {
-		// 	return false;
-		// }
 	    }
     return $associatedPostIds;
 }
 
-function gp_the_associated_organizations($postID) {
+// Get specialty bearing status of associated organizations
+function gp_get_associated_organization_status($postID) {
     $specialty_bearing = array();
+    // Get IDs of associated organizations
     $associatedPostIds = gp_get_associated_organization_ids($postID);
+    // Create array of bearing status of associated organizations
     foreach($associatedPostIds as $id){
         $specialty_bearing[] = (bool) get_post_meta( $id, 'organization_specialty_bearing', true );
     }
+    // Return true if any associated organization has specialty bearing status
     if (in_array(true, $specialty_bearing)) {
         return true;
     } else {
         return false;
     }
-    // return implode(",",$associatedPostIds);
-    // return gp_get_associated_organization_ids($postID);
 }
+
+// Set specialty bearing status of person
+function gp_set_associated_organization_status($post) {
+    $postID = $post->ID;
+    // Get specialty bearing status of associated organizations
+    $specialty_bearing = gp_get_associated_organization_status($postID);
+    // Set post meta for specialty bearing status of person
+    update_post_meta($postID, 'person_specialty_bearing', $specialty_bearing);
+}
+add_action( 'rest_after_insert_person', 'gp_set_associated_organization_status', 100, 1 );
+
+// set specialty bearing status of person after updating associated organizations
+function gp_after_organization_update($post) {
+    // Get post ID of organization
+    $postID = $post->ID;
+  
+    // Get term ID of organization
+    $termID = ShadowTerms\API\get_term_id($postID);
+
+    // Get IDs of associated persons
+    $args = array(
+        'fields' => 'ids',
+        'post_type' => 'person',
+        'post_status' => 'publish',
+        'numberposts' => -1,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'organization_connect',
+                'field' => 'term_id',
+                'terms' => $termID,
+            ),
+        ),
+    );
+    $associatedPostIds = get_posts( $args );
+
+    if ($associatedPostIds) {
+        foreach($associatedPostIds as $postID){
+            // Set specialty bearing status of person
+            gp_set_associated_organization_status(get_post($postID));
+        }
+    }
+}
+add_action( 'rest_after_insert_organization', 'gp_after_organization_update', 100, 1 );
